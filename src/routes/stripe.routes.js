6719bbecbe56870f18873379f947403c595b9a50
Jesus Mendoza Verduzco <loginlock22@gmail.com>
2022-10-04 08:32:05 -0500

    Certificate

--- src/routes/stripe.routes.js
+++ src/routes/stripe.routes.js
@@ -1,12 +1,12 @@
-const { Router } = require("express");
+const express = require("express");
 const bodyParser = require('body-parser');
 const { config, createCheckoutSession, webhook, checkoutsList } = require("../controller/stripe.controller");
-const router = Router();
+const router = express.Router();
 
 router.get("/config", config);
 router.post("/create-checkout-session", createCheckoutSession);
 router.get('/checkout-list', checkoutsList);
-router.post("/webhook", bodyParser.raw({type: 'application/json'}), webhook);
+router.post("/webhook", express.raw({type: 'application/json'}), webhook);
 
 /**
  * Módulo de rutas de stripe

016c637f4cfec602e06cab6ac1a961d18486954c
Jesus Mendoza Verduzco <loginlock22@gmail.com>
2022-10-13 14:41:09 -0500

    google keys

--- src/react-app/config/enviroment.js
+++ src/react-app/config/enviroment.js
@@ -6,14 +6,14 @@ const GLOBAL = {
     XILION_URL: "https://xilion.com.mx",
     NEGOCIO_URL: "https://web.yimiglobal.com/negocio/signup",
 
-    GOOGLE_CLIENT_ID:"752440010120-knqhjit2du8kl5onca06s62av1jmpbuv.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET:"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",
-    GOOGLE_REDIRECT:["http://localhost:3002"],
+    GOOGLE_CLIENT_ID:"752440010120-eohfvatn4qjf5beugq2gd5u9lp9nlguk.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET:"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",
+    GOOGLE_REDIRECT:["https://checkout.yimiglobal.com","http://localhost:3002"],
     project_id:"yimi-1699d",
     auth_uri:"https://accounts.google.com/o/oauth2/auth",
     token_uri:"https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url:"https://www.googleapis.com/oauth2/v1/certs",
-    javascript_origins:["http://localhost:3002"],
+    javascript_origins:["https://checkout.yimiglobal.com","http://localhost:3002"],
 
     FACEBOOK_APP_ID_TEST: "5718470938210426",
     FACEBOOK_APP_SECRET_TEST: "23a82565c524c924d6316d3c73182fbe",

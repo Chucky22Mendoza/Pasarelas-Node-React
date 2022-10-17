601eff13b7b672ebba513c6450457f6f700d9418
Jesus Mendoza Verduzco <loginlock22@gmail.com>
2022-10-17 12:32:14 -0500

    Cambio a producción

--- src/react-app/utils/pricesId.js
+++ src/react-app/utils/pricesId.js
@@ -1,8 +1,8 @@
 const prices = {
-    "plus-month": "price_1LmlndDwc7vlTQP1F02odPht",
-    "plus-anual": "price_1LfrwJDwc7vlTQP1VaqU4fBS",
-    "premium-month": "price_1LmlR0Dwc7vlTQP1HE3uT8ai",
-    "premium-anual": "price_1LmlR0Dwc7vlTQP1sQImPdi2",
+    "plus-month": "price_1Ltv60Dwc7vlTQP1KdMM6DZc",
+    "plus-anual": "price_1LtvFwDwc7vlTQP1TXcDJKI2",
+    "premium-month": "price_1LtvOXDwc7vlTQP19eIIdUSa",
+    "premium-anual": "price_1Lfs7tDwc7vlTQP1lChSujRA",
 };
 
 export default prices;

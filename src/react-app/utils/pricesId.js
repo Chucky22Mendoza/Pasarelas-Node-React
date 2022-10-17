6133438eafdd0cf83a4f19890909891916b04543
Jesus Mendoza Verduzco <loginlock22@gmail.com>
2022-10-17 15:35:26 -0500

    Reset to test

--- src/react-app/utils/pricesId.js
+++ src/react-app/utils/pricesId.js
@@ -5,4 +5,11 @@ const prices = {
     "premium-anual": "price_1Lfs7tDwc7vlTQP1lChSujRA",
 };
 
-export default prices;
+const prices_test = {
+    "plus-month": "price_1LmlndDwc7vlTQP1F02odPht",
+    "plus-anual": "price_1LfrwJDwc7vlTQP1VaqU4fBS",
+    "premium-month": "price_1LmlR0Dwc7vlTQP1HE3uT8ai",
+    "premium-anual": "price_1LmlR0Dwc7vlTQP1sQImPdi2",
+};
+
+export default prices_test;
